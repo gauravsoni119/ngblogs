30eb942fa4ae494ae7374b64e1697d42080eba3e
gauravsoni119 <gauravheart.soni@gmail.com>
2017-03-25 18:21:10 +0530

    add loader

--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,12 @@
 angular.module('ngBlogs', ['ui.router']);
+
+angular.module('ngBlogs')
+    .run(['$rootScope', function($rootScope) {
+        $rootScope.showLoader = false;
+        $rootScope.$on('$stateChangeStart', function() {
+            $rootScope.showLoader = true;
+        });
+        $rootScope.$on('$stateChangeSuccess', function() {
+            $rootScope.showLoader = false;
+        });
+    }]);
